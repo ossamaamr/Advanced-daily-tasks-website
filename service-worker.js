c4e236c5a51a77cfeeb59aa904b0939c2f83b25a
ossamaamr <219485444+ossamaamr@users.noreply.github.com>
2026-02-19 12:49:11 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "task-manager-cache-v1";
+const FILES_TO_CACHE = [
+  "index.html",
+  "style.css",
+  "script.js",
+  "settings.js",
+  "login.html",
+  "login.js",
+  "assets/icon-192.png",
+  "assets/icon-512.png"
+];
+
+// تثبيت Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// جلب الملفات من الكاش
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
